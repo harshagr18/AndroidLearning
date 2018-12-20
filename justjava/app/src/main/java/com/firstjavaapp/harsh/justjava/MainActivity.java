10ba812cf79217e466133718406e2b1aa023c733
Harsh Agrawal <harsh.agrawal17@siesgst.ac.in>
2018-12-20 21:55:24 +0530

    Modified justjava

--- justjava/app/src/main/java/com/firstjavaapp/harsh/justjava/MainActivity.java
+++ justjava/app/src/main/java/com/firstjavaapp/harsh/justjava/MainActivity.java
@@ -4,6 +4,7 @@ package com.firstjavaapp.harsh.justjava;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.CheckBox;
 import android.widget.TextView;
 
 import java.text.NumberFormat;
@@ -19,36 +20,41 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
-int nocoffee=0;
+    int nocoffee=0;
+    int price=5;
+    boolean hasCream;
     /**
      * This method is called when the order button is clicked.
      */
     public void increment(View view) {
     nocoffee=nocoffee+1;
-    display(nocoffee);
+    display(nocoffee,price);
     }
 
     public void submitOrder(View view) {
-        displayPrice(nocoffee*5);
+        CheckBox creamcheck = (CheckBox) findViewById(R.id.creamcheck);
+        hasCream= creamcheck.isChecked();
+        displayPrice(nocoffee,price);
     }
 
     public void decrement(View view) {
         if(nocoffee!=0)
         {nocoffee=nocoffee-1;
-        display(nocoffee);}
+        display(nocoffee,price);}
     }
     /**
      * This method displays the given quantity value on the screen.
      */
-    private void display(int number) {
+    private void display(int number,int price) {
         TextView quantityTextView = (TextView) findViewById(R.id.quantity_no);
         quantityTextView.setText("" + number);
     }
     /**
      * This method displays the given price on the screen.
      */
-    private void displayPrice(int number) {
+    private void displayPrice(int nos,int price) {
+        int number=nos*price;
         TextView priceTextView = (TextView) findViewById(R.id.price_no);
-        priceTextView.setText("TOTAL " + NumberFormat.getCurrencyInstance().format(number)+"\nThank You!");
+        priceTextView.setText("Name:Harsh Agrawal\nHas whipped cream?" + hasCream + "\nQuantity: " + nos + "\nTotal = $" + number+ "\nThank You!" );
     }
 }
