dca2a8ab8059f05a8ae87295087c880c870930d3
Harsh Agrawal <harsh.agrawal17@siesgst.ac.in>
2018-12-24 22:32:43 +0530

    Added email support :)

--- justjava/app/src/main/java/com/firstjavaapp/harsh/justjava/MainActivity.java
+++ justjava/app/src/main/java/com/firstjavaapp/harsh/justjava/MainActivity.java
@@ -1,10 +1,12 @@
 package com.firstjavaapp.harsh.justjava;
 
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.CheckBox;
+import android.widget.EditText;
 import android.widget.TextView;
 
 import java.text.NumberFormat;
@@ -23,22 +25,30 @@ public class MainActivity extends AppCompatActivity {
     int nocoffee=0;
     int price=5;
     boolean hasCream;
+    boolean hasChoco;
+    String hisname;
+    String ordermail;
     /**
      * This method is called when the order button is clicked.
      */
     public void increment(View view) {
-    nocoffee=nocoffee+1;
-    display(nocoffee,price);
+        if(nocoffee<=100)
+        {nocoffee=nocoffee+1;
+    display(nocoffee,price);}
     }
 
     public void submitOrder(View view) {
+        EditText name = (EditText) findViewById(R.id.name);
+        hisname= name.getText().toString();
         CheckBox creamcheck = (CheckBox) findViewById(R.id.creamcheck);
         hasCream= creamcheck.isChecked();
+        CheckBox chocochek = (CheckBox) findViewById(R.id.chococheck);
+        hasChoco= chocochek.isChecked();
         displayPrice(nocoffee,price);
-    }
+        }
 
     public void decrement(View view) {
-        if(nocoffee!=0)
+        if(nocoffee>1)
         {nocoffee=nocoffee-1;
         display(nocoffee,price);}
     }
@@ -52,9 +62,23 @@ public class MainActivity extends AppCompatActivity {
     /**
      * This method displays the given price on the screen.
      */
-    private void displayPrice(int nos,int price) {
+    public void displayPrice(int nos,int price) {
+        if(hasCream==true)
+        {
+            price=price+1;
+        }
+        if(hasChoco==true)
+        {
+            price=price+2;
+        }
         int number=nos*price;
-        TextView priceTextView = (TextView) findViewById(R.id.price_no);
-        priceTextView.setText("Name:Harsh Agrawal\nHas whipped cream?" + hasCream + "\nQuantity: " + nos + "\nTotal = $" + number+ "\nThank You!" );
+        Intent intent = new Intent(Intent.ACTION_SEND);
+        intent.setType("text/html");
+        intent.putExtra(Intent.EXTRA_EMAIL, "agrawalco.9@gmail.com");
+        intent.putExtra(Intent.EXTRA_SUBJECT, "Trying to order");
+        ordermail = ("Name: "+hisname+"\nHas whipped cream?" + hasCream +"\nHas chocolate Toppings? " + hasChoco + "\nQuantity: " + nos + "\nTotal = $" + number+ "\nThank You!" );
+        intent.putExtra(Intent.EXTRA_TEXT, ordermail);
+        startActivity(Intent.createChooser(intent, "Send Email"));
+
     }
 }
